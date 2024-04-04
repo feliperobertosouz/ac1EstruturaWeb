d97e48bfde226221e362abd3ae81342c9bc9d8aa
Felipe <feliperobertosouz@gmail.com>
2024-04-04 18:26:54 -0300

    fixed Task entity

--- ac1/src/main/java/me/felipe/ac1/model/Task.java
+++ ac1/src/main/java/me/felipe/ac1/model/Task.java
@@ -1,13 +1,12 @@
 package me.felipe.ac1.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "task")
 public class Task {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String description;
